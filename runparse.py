3b7d9851fa677883a4c7278a3b52ee3e680b9656
Ivan <ivan.maliyov@gmail.com>
2021-10-06 19:14:45 -0700

    First steps with pygraphviz

--- runparse.py
+++ runparse.py
@@ -5,6 +5,7 @@ Run the code parsing and create the subroutine/module interactive graphs
 """
 
 import os, sys
+import pygraphviz as pgv
 import textwrap
 import argparse
 
@@ -34,7 +35,11 @@ Usage examples:
    runparse.py -p ../../pert-src -m     Parse the perturbo.x source code and build the subroutine and module trees
    """
 
-def get_callable_dict(path,source_file_list):
+def create_callable_dict(path,source_file_list):
+   """
+   Create a dictionary of the calls (functions, subroutines, and interfaces) 
+   from all the files from source_file_list
+   """
    #
    # fparser provides a parse tree per file.
    # So here, we run over all the files of the source directory and create a dictionary of parse trees
@@ -72,6 +77,27 @@ def get_callable_dict(path,source_file_list):
 
    return callable_dict
 
+def create_callable_graph_dict(callable_dict):
+   """
+   Using the callable_dict, create a dict that can be processed by pygraphviz
+   """
+   t1 = tnow()
+   print('\nCreating a callable graph')
+
+   graph_dict = {}
+
+   for name, obj in callable_dict.items():
+
+      tmp_call_dict = {}
+      for call in obj.calls: 
+         tmp_call_dict[call] = None
+
+      graph_dict[name] = tmp_call_dict
+
+   print('\nDone: {:.1f} s'.format(tnow() - t1))
+
+   return graph_dict
+
 def main():
    #
    # Parse command line arguments
@@ -99,18 +125,53 @@ def main():
 
    source_file_list = sorted(source_file_list)
 
-   callable_dict = get_callable_dict(args.path,source_file_list)
+   callable_dict = create_callable_dict(args.path,source_file_list)
 
+   #
+   # Test
+   #
    #obj = callable_dict['init_boltz_grid']
-   obj = callable_dict['transport']
+   #obj = callable_dict['transport']
+   #obj = callable_dict['hdf_write_dataset']
    #obj = callable_dict['hdf_read_attr_integer_1']
-   print_object_attributes(obj)
+   #print_object_attributes(obj)
 
    # maybe, the built in comparison works...
    #print(callable_dict['transport']==callable_dict['transport'])
 
    # tostr() takes into account the comments that are before!
    #print(obj._node.parent.tostr())
+
+   #
+   # Create the graph dict for callables
+   #  
+
+   #def get_all_successors(graph,node):
+      #yield graph.successors()
+     
+   graph_dict = create_callable_graph_dict(callable_dict)
+
+   call_graph = pgv.AGraph(graph_dict,strict=False,directed=True)#.reverse()
+
+   print(call_graph.successors(call_graph.get_node('init_boltz_grid')))
+   root_node_name = 'transport'
+   root_node = call_graph.get_node(root_node_name)
+
+   #
+   # Remove all the nodes that are not the successors of the root node
+   #
+   successors = call_graph.successors(root_node)
+
+   for node in call_graph.nodes():
+      if node not in successors and node != root_node:
+         call_graph.delete_node(node)
+
+   call_graph.node_attr['shape']='rectangle'
+   call_graph.graph_attr['rankdir']='LR'
+   call_graph.layout(prog='dot')
+
+   call_graph.draw('graph.png')
+
 
 if __name__ == '__main__':
    main()
